0fea22053f4aca9f96704042e9cbed34c22f1f8a
Ishan Sodani <ishansodani53@gmail.com>
2024-04-05 16:03:17 +0530

    first commit

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+const JWT_SECRET="12287dehedh378387348734873487"
+const generateTokenAndSetCookie = (userId, res) => {
+	const token = jwt.sign({ userId }, JWT_SECRET, {
+		expiresIn: "15d",
+	});
+
+	res.cookie("jwt", token, {
+		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
+		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+		sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+		secure: process.env.NODE_ENV !== "development",
+	});
+};
+
+export default generateTokenAndSetCookie;
